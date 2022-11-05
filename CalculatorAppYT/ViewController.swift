5cc294c2f8392a013e985377eaafadbf714be004
burak <burak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.com>
2022-11-05 16:30:05 +0300

    first

--- CalculatorAppYT/ViewController.swift
+++ CalculatorAppYT/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var result: UITextView!
+    
+    var resultNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func subClick(_ sender: Any) {
+       if let firstText = Int(firstNumber.text!){
+           if let secondText = Int(secondNumber.text!){
+               resultNumber = firstText - secondText
+               result.text = String(resultNumber)
+           }
+        }
+        
+    }
+    
+    @IBAction func plusClick(_ sender: Any) {
+        if let firstText = Int(firstNumber.text!){
+            if let secondText = Int(secondNumber.text!){
+                resultNumber = firstText + secondText
+                result.text = String(resultNumber)
+            }
+         }
+    }
+    
+    @IBAction func multiClick(_ sender: Any) {
+        if let firstText = Int(firstNumber.text!){
+            if let secondText = Int(secondNumber.text!){
+                resultNumber = firstText * secondText
+                result.text = String(resultNumber)
+            }
+         }
+    }
+    @IBAction func divideClick(_ sender: Any) {
+        if let firstText = Int(firstNumber.text!){
+            if let secondText = Int(secondNumber.text!){
+                resultNumber = firstText / secondText
+                result.text = String(resultNumber)
+            }
+         }
+    }
+    
 }
 
